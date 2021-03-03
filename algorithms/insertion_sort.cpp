cb53a98c6e2d229069986bb5c5d1277b87c3248a
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2021-03-03 17:00:21 +0300

    Folder for algorithms
    Insertion sort

---
+++ algorithms/insertion_sort.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Dmitry Kochetkov on 3/3/2021.
+//
+
+#include <iostream>
+
+void insertion_sort(int len, int* a) {
+    for (int i = 1; i < len; i++) {
+        int key = a[i];
+        int j = i-1; //
+        while (j >= 0 && a[j] > key) {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = key;
+    }
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int* a = new int(n);
+    for (int i = 0; i < n; i++)
+        std::cin >> a[i];
+
+    insertion_sort(n, a);
+    for (int i = 0; i < n; i++)
+        std::cout << a[i] << std::endl;
+
+    delete a;
+    return 0;
+}
